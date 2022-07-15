56f356928de2317e491fa9cc5bee9c1f01f748e2
camilevahviraki <camilevahviraki@gmail.com>
2022-07-15 16:19:58 +0200

    remove inline comment

--- my_list.rb
+++ my_list.rb
@@ -1,7 +1,6 @@
 require_relative 'my_enumerable'
 
 class MyList
-  # include module
   include MyEnumerable
 
   def initialize(*args)
@@ -11,9 +10,7 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-# test cases
 p(list.all? { |e| e < 5 })
 p(list.all? { |e| e > 5 })
 p(list.any? { |e| e == 2 })
 p(list.any? { |e| e == 5 })
-# p(list.filter { |e| e.even? })
