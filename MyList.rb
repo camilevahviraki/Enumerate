e32bdf37e8b9b746556be0b996b9b2d763792032
camilevahviraki <camilevahviraki@gmail.com>
2022-07-15 14:51:26 +0200

    Include module in MyList class

--- MyList.rb
+++ MyList.rb
@@ -1,35 +1,14 @@
+require_relative 'MyEnumerable'
+
 class MyList
+
+  # include module
+  include MyEnumerable
+
   def initialize(*args)
     @list = args
   end
 
-  def all?
-    @list.each do |el|
-      return true unless block_given?
-      true_false = yield el
-      return false unless true_false
-    end
-    return true
-  end
-  
-  def any?
-    @list.each do |l|
-      if yield l
-        return true
-      end
-    end
-    false
-  end
-  
-  def filter
-    arr = []
-    @list.each do |l|
-      if yield l
-        arr.push(l)
-      end
-    end
-    arr
-  end
 end
 
 list = MyList.new(1,2,3,4)
